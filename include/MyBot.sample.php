2d509d66b3c2b89d22fba7a5c43a46af4e5c3ed1
Steve Ousley <steve.ousley@gmail.com>
2013-09-25 13:32:37 +0100

    .

---
+++ include/MyBot.sample.php
@@ -0,0 +1,13 @@
+<?php
+
+$conf = array(
+    'COMMAND_PREFIX' => '!',
+    'SERVER' => array(
+        'hostname' => 'irc.freenode.net',
+        'password' => '',
+        'nickname' => 'MyPHPBot',
+        'channels' => array(
+            '#sdousley',
+        ),
+    )
+);
